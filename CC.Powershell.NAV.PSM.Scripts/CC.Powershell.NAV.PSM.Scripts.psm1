f51dfcf9c940b4dcfebf68028d1a0c40ed2d881c
Mathias Stjernfelt <32168863+mstjernfelt@users.noreply.github.com>
2018-03-06 14:42:09 +0100

    Removed files

--- CC.Powershell.NAV.PSM.Scripts/CC.Powershell.NAV.PSM.Scripts.psm1
+++
@@ -1,6 +0,0 @@
-﻿Get-Item $PSScriptRoot | Get-ChildItem -Recurse -Include '*.ps1' |  Sort Name | foreach {
-    Write-Verbose "Loading $($_.Name)"  
-    . $_.fullname
-}
-
-Export-ModuleMember -Function *
